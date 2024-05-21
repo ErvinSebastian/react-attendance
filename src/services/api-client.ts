7c8bbf82a6c569dece05f0ff079038282ce111d0
ErvinSebastian <44946513+ErvinSebastian@users.noreply.github.com>
2024-05-21 21:24:41 +0800

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios, { CanceledError } from 'axios';
 
 export default axios.create({
-  baseURL: 'https://qrize-app-online.preview-domain.com/index.php',
+  baseURL: 'https://qrize-app.online/index.php',
   headers: {
     'Access-Control-Allow-Origin': '*',
    
